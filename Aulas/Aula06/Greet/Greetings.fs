7601d3893e561b2488eef002006689d769d11cbc
Fabio Ayres <fabioja@insper.edu.br>
2026-03-05 19:37:46 +0000

    feat: solutions of Aula06

--- Aulas/Aula06/Greet/Greetings.fs
+++ Aulas/Aula06/Greet/Greetings.fs
@@ -1,1 +1,21 @@
+namespace Greetings
 
+[<RequireQualifiedAccess>]
+module Greetings =
+
+    let makeHello (vip: string list) (name: string) : string =
+        if List.contains name vip then
+            $"Welcome, {name}!"
+        else
+            $"Hello, {name}."
+
+    type DayPeriod =
+        | Morning
+        | Afternoon
+        | Evening
+
+    let salute (period: DayPeriod) : string =
+        match period with
+        | Morning -> "Good Morning!"
+        | Afternoon -> "Good Afternoon!"
+        | Evening -> "Good Evening!"
